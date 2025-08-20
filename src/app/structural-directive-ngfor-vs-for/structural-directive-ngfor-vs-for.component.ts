1d3f5be55fe12a7b78bd8b7e67e66432341c4e74
Sanskar Kaushik <sanskar.kaushik3901@gmail.com>
2025-08-20 12:12:26 +0530

    ngFor vs @for()

--- src/app/structural-directive-ngfor-vs-for/structural-directive-ngfor-vs-for.component.ts
+++ src/app/structural-directive-ngfor-vs-for/structural-directive-ngfor-vs-for.component.ts
@@ -1,11 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-structural-directive-ngfor-vs-for',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './structural-directive-ngfor-vs-for.component.html',
-  styleUrl: './structural-directive-ngfor-vs-for.component.css'
+  styleUrl: './structural-directive-ngfor-vs-for.component.css',
 })
 export class StructuralDirectiveNgforVsForComponent {
-
+  employeeDetails: any[] = [
+    {
+      empName: 'Sanskar',
+      empNum: 96,
+      empEmail: 'sanskar@gmail.com',
+      empDept: 'developer',
+    },
+    {
+      empName: 'Devesh',
+      empNum: 97,
+      empEmail: 'devesh@gmail.com',
+      empDept: 'HR',
+    },
+    {
+      empName: 'Kitanshi',
+      empNum: 98,
+      empEmail: 'kitanshi@gmail.com',
+      empDept: 'AI',
+    },
+    {
+      empName: 'Priyanshu',
+      empNum: 99,
+      empEmail: 'priyanshu@gmail.com',
+      empDept: 'infra',
+    },
+  ];
+  empNames: string[] = ['Sanskar', 'Priyanshu', 'Celina', 'Deepak'];
 }
