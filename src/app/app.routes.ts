a16264128b999a054e36ed8b3bde36ddce95df1e
Sanskar Kaushik <sanskar.kaushik3901@gmail.com>
2025-09-06 19:48:11 +0530

    Custom Pipes 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { DataBindingComponent } from './Components/data-binding/data-binding.com
 import { StructuralDirectiveNgIfVsIfComponent } from './Components/structural-directive-ng-if-vs-if/structural-directive-ng-if-vs-if.component';
 import { AttributeDirectivesComponent } from './Components/attribute-directives/attribute-directives.component';
 import { PageNotFoundComponent } from './Components/page-not-found/page-not-found.component';
-import { InbuiltPipesComponent } from './inbuilt-pipes/inbuilt-pipes.component';
+import { InbuiltPipesComponent } from './Components/inbuilt-pipes/inbuilt-pipes.component';
 
 export const routes: Routes = [
   { path: '' , redirectTo: "user" , pathMatch:"full"},
