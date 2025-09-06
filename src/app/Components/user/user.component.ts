509fe69b867513272b4831c67f53ef3666cfd0b7
Sanskar Kaushik <sanskar.kaushik3901@gmail.com>
2025-09-06 10:16:02 +0530

    Lifecycle hooks

--- src/app/Components/user/user.component.ts
+++ src/app/Components/user/user.component.ts
@@ -1,4 +1,15 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
@@ -6,10 +17,46 @@ import { Router } from '@angular/router';
   selector: 'app-user',
   imports: [FormsModule],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
-export class UserComponent {
-  constructor(private router: Router) {}
+export class UserComponent
+  implements
+    OnChanges,
+    OnInit,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+{
+  constructor(private router: Router) {
+    console.log('0. Constructor called');
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('1. ngOnChanges called');
+  }
+  ngOnInit(): void {
+    console.log('2. ngOnInit called');
+  }
+  ngDoCheck(): void {
+    console.log('3. ngOnCheck called');
+  }
+  ngAfterContentInit(): void {
+    console.log('4. ngAfterContentInit called');
+  }
+  ngAfterContentChecked(): void {
+    console.log('5. ngAfterContentChecked called');
+  }
+  ngAfterViewInit(): void {
+    console.log('6. ngAfterViewInit called');
+  }
+  ngAfterViewChecked(): void {
+    console.log('7. ngAfterViewChecked called');
+  }
+  ngOnDestroy(): void {
+    console.log('8. ngOnDestroy called');
+  }
   username: string = '';
   password: string = '';
   loginData() {
