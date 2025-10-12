6aef3ff3b672516661ebe307773d3bb872334b01
Sanskar Kaushik <sanskar.kaushik3901@gmail.com>
2025-10-12 19:38:08 +0530

    Crud Operations

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,11 +8,19 @@ import { InbuiltPipesComponent } from './Components/inbuilt-pipes/inbuilt-pipes.
 import { TemplateDrivenFormComponent } from './Components/template-driven-form/template-driven-form.component';
 import { ReactiveFormsComponent } from './Components/reactive-forms/reactive-forms.component';
 import { ProfileComponent } from './Components/profile/profile.component';
+import { CrudComponent } from './Components/crud/crud.component';
+import { AddUserComponent } from './Components/add-user/add-user.component';
+import { UpdateUserComponent } from './Components/update-user/update-user.component';
+import { ViewUserComponent } from './Components/view-user/view-user.component';
 
 export const routes: Routes = [
   { path: '' , redirectTo: "user" , pathMatch:"full"},
   { path: 'user', component: UserComponent },
   { path: 'profile', component: ProfileComponent },
+  { path: 'crud', component: CrudComponent },
+  { path: 'addUser', component: AddUserComponent },
+  { path: 'updateUser/:id', component: UpdateUserComponent },
+  { path: 'viewUser/:id', component: ViewUserComponent },
   { path: 'data-binding', component: DataBindingComponent },
   { path: 'structural-directives', component: StructuralDirectiveNgIfVsIfComponent},
   { path: 'attribute-directives', component: AttributeDirectivesComponent },
